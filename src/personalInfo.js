64902a13193fb82fcf268ca7c13153f145d419f9
Adrian Bogdan <enu999@o2.pl>
2023-06-01 18:08:24 +0200

    src personalInfo.js added

---
+++ src/personalInfo.js
@@ -0,0 +1,39 @@
+export const email = "BogdanAdrian796@gmail.com";
+export const github = "https://github.com/AdrianBogdan796";
+export const linkedin = "https://www.linkedin.com/in/adrian-bogdan-78718a279/";
+
+export const personalInfo = [
+  {
+    title: "My skillset includes",
+    icon: "🛠️",
+    list: [
+      "Working with API (axios, fetch)",
+      "JavaScript ES6+",
+      "React",
+      "Redux-Saga",
+      "Redux (Toolkit)",
+      "React Hooks",
+      "React Router",
+      "Semantic & accessible HTML",
+      "Responsive Web Design",
+      "CSS BEM convention",
+      "CSS Grid",
+      "CSS Flexbox",
+      "Teamwork",
+      "Markdown",
+      "Immutability",
+      "Error handling",
+      "Promises, Async/Await",
+      "Git",
+      "GitHub Pull Requests & Review",
+      "Trello",
+      "Scrum",
+      "NPM",
+    ],
+  },
+  {
+    title: "What i want to learn next",
+    icon: "🚀",
+    list: [""],
+  },
+];
