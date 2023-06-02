2ae939935bf70312ce4005df226536d5cf742e4e
Adrian Bogdan <enu999@o2.pl>
2023-06-02 16:26:56 +0200

    App/index.js

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,13 +1,13 @@
-import SectionHeader from "../../components/SectionHeader/index.js";
-import SectionSkills from "../../components/SectionSkills/index.js";
-import SectionPortfolio from "../../components/SectionPortfolio/index.js";
-import Footer from "../../components/Footer/index.js";
-import { AppContainer } from "../../components/Container/styled.js";
+import SectionHeader from "../../common/SectionHeader/index.js";
+import SectionSkills from "../../common/SectionSkills/index.js";
+import SectionPortfolio from "../../common/SectionPortfolio/index.js";
+import Footer from "../../common/Footer/index.js";
+import { AppContainer } from "../../common/Container/styled.js";
 import { GlobalStyles } from "./GlobalStyles.js";
 import { Normalize } from "styled-normalize";
 import { ThemeProvider } from "styled-components";
 import { useSelector } from "react-redux";
-import { selectIsDarkTheme } from "../../components/SectionHeader/ThemeSwitch/themeSlice.js";
+import { selectIsDarkTheme } from "../../common/SectionHeader/ThemeSwitch/themeSlice.js";
 import { darkTheme, lightTheme } from "./theme.js";
 
 function App() {
