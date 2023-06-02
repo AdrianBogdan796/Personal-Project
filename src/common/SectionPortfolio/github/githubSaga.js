a025eff917cbf70d8d5c71f15447cec98973cc38
Adrian Bogdan <enu999@o2.pl>
2023-06-02 16:23:57 +0200

    github added

---
+++ src/common/SectionPortfolio/github/githubSaga.js
@@ -0,0 +1,22 @@
+import { call, delay, put, takeLatest } from "redux-saga/effects";
+import { getRepos } from "../getRepos";
+import {
+  fetchGithubRepos,
+  fetchGithubReposSuccess,
+  fetchGithubReposError,
+} from "./githubSlice";
+
+function* fetchGithubHandler() {
+  try {
+    yield delay(1000);
+    const repos = yield call(getRepos);
+    yield put(fetchGithubReposSuccess(repos));
+  } catch (error) {
+    yield put(fetchGithubReposError(error));
+    yield call(console.error);
+  }
+}
+
+export function* githubSaga() {
+  yield takeLatest(fetchGithubRepos.type, fetchGithubHandler);
+}
