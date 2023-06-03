32d271c06d897729175cee58e77af8f8d5140eb4
Adrian Bogdan <enu999@o2.pl>
2023-06-03 15:55:01 +0200

    added

--- src/common/SectionPortfolio/Tile/index.js
+++ src/common/SectionPortfolio/Tile/index.js
@@ -7,7 +7,7 @@ import {
   Link,
 } from "./styled";
 
-const GitHubTile = ({ name, descriprion, gitUrl, demo }) => {
+const GitHubTile = ({ name, description, gitUrl, demo }) => {
   const formattedName = (name[0].toUpperCase() + name.substring(1)).replaceAll(
     "-",
     " "
@@ -16,7 +16,7 @@ const GitHubTile = ({ name, descriprion, gitUrl, demo }) => {
   return (
     <WrapperTile title={`repository name: ${name}`}>
       <Title>{formattedName}</Title>
-      <Descriprion>{descriprion}</Descriprion>
+      <Descriprion>{description}</Descriprion>
       {demo && (
         <LinksWrapper>
           <dt>Demo:</dt>
